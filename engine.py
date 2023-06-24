8db00b8eaef99914b85365db5ce9bd438b21719a
Tai-Yu Chen <86171033+prince-of-tennis@users.noreply.github.com>
2023-06-24 00:30:29 -0700

    clarity changes

--- engine.py
+++ engine.py
@@ -14,14 +14,13 @@ INVALID = 3
 currMinutes = 0
 currHours = 0
 runtime = '0:00'
-imageName = 'Star Guardians Ubuntu 20'
+imageName = 'Image Name'
 name = ''
 
 
 # configuration files; opens file path, creates list using delimiters, checks required configs
 
 class ConfigObject:
-
     path = ''
     key = ''
     entry = ''
@@ -262,22 +261,8 @@ def writeScores(
 vulns = []
 
 '''
-Add your vulns here, below is some sample code you can edit
-
-vulns.append(newCommandObject('cat /home/kaisa/Desktop/Forensics_1.txt'
-             , 'Valoran City Park', True, 6,
-             'Forensics Question 1 correct'))
-vulns.append(newCommandObject('cat /etc/passwd | grep -v "#" | grep zoe | wc -l'
-             , '1', False, 2, 'Removed unauthorized user zoe'))
-vulns.append(newCommandObject('cat /etc/shadow | grep lux',
-             '$6$lpSQPhyvEux4vqN7$5CcsHwirFEhvgFqT8sAojdISP/9wGfdCjHfMZ1aH3xkUV4ze3tQtsyOYYB7bbPYylgcjD86hOuNu08nIAu1Un0:19170'
-             , False, 2, 'Changed insecure password for lux'))
-vulns.append(newCommandObject('apt list --installed goldeneye',
-             'installed', False, 2,
-             'Prohibited software goldeneye removed'))
-
+Add your vulns here
 '''
-
 
 # checking vulns
 
